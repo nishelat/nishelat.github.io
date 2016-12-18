1b2d2908e971e9ee91ede5f56f644324ad77f89d
Nishir <nshelat@usc.edu>
2016-12-18 10:35:28 -0800

    Remove banner fade

--- script.js
+++ script.js
@@ -2,13 +2,11 @@ var clicked = 0;
 
 document.getElementById('menuIMG').addEventListener('click', function() {
 	if(clicked == 1) {
-		document.getElementById('header').style.display = "block";
 		document.getElementById('mobileNav').style.display = "none";
 		document.getElementById('menuIMG').style.opacity = "1";
 		clicked = 0;
 	}
 	else {
-		document.getElementById('header').style.display = "none";
 		document.getElementById('mobileNav').style.display = "block";
 		document.getElementById('menuIMG').style.opacity = "0.5";
 		clicked = 1;
